3c3bfb5697405964ff2d926b8afff8a827bd1790
Dor <56084381+MashdorDev@users.noreply.github.com>
2024-03-05 21:29:40 -0500

    Deploy site

--- _symbols/6b4c5947-bc5d-4170-a361-208e9d9ad6a6.js
+++ _symbols/6b4c5947-bc5d-4170-a361-208e9d9ad6a6.js
@@ -1261,7 +1261,7 @@ function get_each_context(ctx, list, i) {
 	return child_ctx;
 }
 
-// (63:6) {#each site_nav as { link }}
+// (67:6) {#each site_nav as { link }}
 function create_each_block(ctx) {
 	let a;
 	let li;
@@ -1290,7 +1290,7 @@ function create_each_block(ctx) {
 			this.h();
 		},
 		h() {
-			attr(li, "class", "svelte-k31pq8");
+			attr(li, "class", "svelte-7u46sh");
 			attr(a, "class", "link");
 			attr(a, "href", a_href_value = /*link*/ ctx[5].url);
 		},
@@ -1335,7 +1335,7 @@ function create_fragment(ctx) {
 			this.h();
 		},
 		l(nodes) {
-			header = claim_element(nodes, "HEADER", { class: true });
+			header = claim_element(nodes, "HEADER", {});
 			var header_nodes = children(header);
 			ul = claim_element(header_nodes, "UL", { class: true });
 			var ul_nodes = children(ul);
@@ -1349,8 +1349,7 @@ function create_fragment(ctx) {
 			this.h();
 		},
 		h() {
-			attr(ul, "class", "menu-bar svelte-k31pq8");
-			attr(header, "class", "section-container");
+			attr(ul, "class", "menu-bar svelte-7u46sh");
 		},
 		m(target, anchor) {
 			insert_hydration(target, header, anchor);
